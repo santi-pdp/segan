7ebf6164d7ea89b80ed52dc354c56a8810aa7899
santi pascual <santi.pdp@gmail.com>
2017-03-29 07:18:28 +0400

    refactor: train save path to segan_v1

--- train_segan.sh
+++ train_segan.sh
@@ -3,6 +3,6 @@
 # Place the CUDA_VISIBLE_DEVICES="xxxx" required before the python call
 # e.g. to specify the first two GPUs in your system: CUDA_VISIBLE_DEVICES="0,1" python ...
 
-python main.py --init_noise_std 0. --save_path segan_checkpoint \
+python main.py --init_noise_std 0. --save_path segan_v1 \
                --init_l1_weight 100. --batch_size 100 --g_nl prelu \
                --save_freq 50
